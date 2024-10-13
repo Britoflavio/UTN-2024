e37d6a6b9ed694402bd36b132d0d05ee2a7f6430
Flavio <flabrito43@gmail.com>
2024-10-13 17:12:15 -0300

    practica-p4

--- PARCIALES/Parcial-4/Turno-2/funciones.py
+++ PARCIALES/Parcial-4/Turno-2/funciones.py
@@ -49,3 +49,10 @@ def ordenar_op_a(v, libro):
     if izq > der:
         x = izq
     v[x:x] = [libro]
+
+def opcion_b(v):
+    idiomas = ['esp', 'ingles', 'port', 'frances', 'italiano']
+    for i in range(len(v)):
+        n = v[i].idioma
+        v[i].idioma = idiomas[n - 1]
+        print(v[i])
